dedbcf5e7229ab450a81e431f6349c4702ac7a02
DannyFung97 <dannyfung97@gmail.com>
2025-02-03 13:13:00 -0800

    added blockchain folder

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,31 @@
+import { HardhatUserConfig, vars } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
+const BASE_SEPOLIA_PRIVATE_KEY = vars.get("BASE_SEPOLIA_PRIVATE_KEY");
+
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+    baseSepolia: {
+      url: `https://base-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [BASE_SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+};
+
+export default config;
